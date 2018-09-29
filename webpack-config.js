29e96f7081d8df42c42f6ecad2f29183866f5148
nainemom <nainemom@gmail.com>
2018-09-29 19:45:03 +0330

    update configs

--- webpack-config.js
+++ webpack-config.js
@@ -1,52 +1,49 @@
 #!/usr/bin/env node
 const path = require('path')
-const utils = require('./build/utils.js')
+const utils = require('./lib/utils.js')
 const argv = require('./lib/argv.js')
 
-  process.env.NODE_ENV = process.env.NODE_ENV || 'production'
-  const configPath = argv.get('--config') || argv.get('-c')
-  const framevuerkDir = path.resolve(process.cwd(), argv.get('--dir') || argv.get('-d') || './node_modules/framevuerk')
-  const framevuerkPkg = require(path.resolve(framevuerkDir, './package.json'))
+process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+const configPath = argv.get('--config') || argv.get('-c')
+const framevuerkDir = path.resolve(process.cwd(), argv.get('--dir') || argv.get('-d') || './node_modules/framevuerk')
+const userOutputDir = argv.get('--output-dir') || argv.get('-o')
+const outputDir = userOutputDir ? path.resolve(process.cwd(), userOutputDir) : path.resolve(framevuerkDir, './dist')
+const framevuerkPkg = require(path.resolve(framevuerkDir, './package.json'))
 
-  const defaultConfig = {
-    'locale': 'en',
-    'direction': 'ltr',
-    'primary-color': '#41b883',
-    'secondary-color': '#35485d',
-    'danger-color': '#dd4b39',
-    'warning-color': '#ef8f00',
-    'info-color': '#14b0cf',
-    'bg-color': '#fff',
-    'header-bg-color': '#35485d',
-    'sidebar-bg-color': '#3a3a3a',
-    'footer-bg-color': '#3a3a3a',
-    'padding': '15px',
-    'transition-speed': '0.35s',
-    'border-radius': '2px',
-    'shadow-size': '1px'
-  }
-  let configs = [defaultConfig]
-  if (configPath) {
-    const configContent = require(path.resolve(process.cwd(), configPath))
-    const parsedConfigContent = Array.isArray(configContent) ? configContent : [configContent]
-    configs = parsedConfigContent.map(config => Object.assign(defaultConfig, config))
-  }
+const defaultConfig = {
+  'locale': 'en',
+  'direction': 'ltr',
+  'primary-color': '#41b883',
+  'secondary-color': '#35485d',
+  'danger-color': '#dd4b39',
+  'warning-color': '#ef8f00',
+  'info-color': '#14b0cf',
+  'bg-color': '#fff',
+  'header-bg-color': '#35485d',
+  'sidebar-bg-color': '#3a3a3a',
+  'footer-bg-color': '#3a3a3a',
+  'padding': '15px',
+  'transition-speed': '0.35s',
+  'border-radius': '2px',
+  'shadow-size': '1px'
+}
+let configs = [defaultConfig]
+if (configPath) {
+  const configContent = require(path.resolve(process.cwd(), configPath))
+  const parsedConfigContent = Array.isArray(configContent) ? configContent : [configContent]
+  configs = parsedConfigContent.map(config => Object.assign(defaultConfig, config))
+}
 
-  const webpackConfigs = []
-  configs.forEach(config => {
-    const cnf = Object.assign(config, {
-      name: framevuerkPkg.name,
-      version: framevuerkPkg.version,
-      rootDir: path.resolve(framevuerkDir),
-      entry: path.resolve(framevuerkDir, './src/index.js'),
-      path: path.resolve(framevuerkDir, './dist')
-    })
-    webpackConfigs.push(utils.generateConfig(Object.assign(cnf, {
-      minify: false
-    })))
-    webpackConfigs.push(utils.generateConfig(Object.assign(cnf, {
-      minify: true
-    })))
-  })
+const webpackConfigs = []
+configs.forEach(config => {
+  webpackConfigs.push(utils.generateConfig(Object.assign(config, {
+    name: framevuerkPkg.name,
+    version: framevuerkPkg.version,
+    framevuerkDir,
+    entry: path.resolve(framevuerkDir, './src/index.js'),
+    path: outputDir,
+    watch: argv.is('-w') || argv.is('--watch')
+  })))
+})
 
-  module.exports = webpackConfigs
+module.exports = webpackConfigs
