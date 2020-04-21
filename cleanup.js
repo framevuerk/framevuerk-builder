22aeb94c60bab5eda760a90fde076963ac50eabb
Amir Momenian <nainemom@gmail.com>
2020-04-21 13:06:55 +0430

    change file path

--- cleanup.js
+++ cleanup.js
@@ -1,9 +1,9 @@
 const path = require('path');
 var fs = require('fs-extra');
-
+console.log('cleaning...');
 fs.moveSync(
+  path.resolve(__dirname, './deps-dir/node_modules'),
   path.resolve(__dirname, './node_modules'),
-  path.resolve(__dirname, '../node_modules'),
   {
     overwrite: true
   },
